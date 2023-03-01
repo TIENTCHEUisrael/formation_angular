4efc0f0602e42301c8872e2fe1c30fef9f54f666
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-03-01 14:27:58 +0100

    Ajout des images d'hotels et les evenements sur la barre de filtre

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -13,53 +13,54 @@ export class HotelListComponent{
       hotelName:'Buea sweet life',
       description:'Belle vue sur la plage',
       price:220.5,
-      imageUrl:'assets/img/hotel-room.jpg'
+      imageUrl:'assets/img/h1.jpg'
     },
     {
       hotelId:2,
       hotelName:'Buea sweet',
       description:'Belle vue sur la plage',
       price:210.5,
-      imageUrl:'assets/img/hotel-room.jpg'
+      imageUrl:'assets/img/h2.jpeg'
     },
     {
       hotelId:3,
       hotelName:'Buea  life',
       description:'Belle vue sur la plage',
       price:200.5,
-      imageUrl:'assets/img/hotel-room.jpg'
+      imageUrl:'assets/img/h3.jpeg'
     },
     {
       hotelId:4,
       hotelName:' sweet life',
       description:'Belle vue sur la plage',
       price:90.5,
-      imageUrl:'assets/img/hotel-room.jpg'
+      imageUrl:'assets/img/h4.jpg'
     },
     {
       hotelId:5,
       hotelName:'Buea swe life',
       description:'Belle vue sur la plage',
       price:90.5,
-      imageUrl:'assets/img/hotel-room.jpg'
+      imageUrl:'assets/img/h5.jpg'
     },
     {
       hotelId:6,
       hotelName:'Bu sweet life',
       description:'Belle vue sur la plage',
       price:330.5,
-      imageUrl:'assets/img/hotel-room.jpg'
+      imageUrl:'assets/img/h6.jpeg'
     },
     {
       hotelId:7,
       hotelName:'Bea sweet li',
       description:'Belle vue sur la plage',
       price:450.5,
-      imageUrl:'assets/img/hotel-room.jpg'
+      imageUrl:'assets/img/h1.jpg'
     }
   ]
 
   public showBadge:boolean=false;
+  public hotelFilter="mot";
 
   public toggleIsNewBadge():void{
     this.showBadge=!this.showBadge;
